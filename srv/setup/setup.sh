f9902685fd1fc6d97764498b54f78cf5f86c553f
kumanoryo <r-kumano@groovenauts.jp>
2017-05-09 18:51:42 +0900

    Create setup dir and move setup files.

--- srv/setup/setup.sh
+++ srv/setup/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-WORK_DIR=/srv
+WORK_DIR=/srv/setup
 
 sudo apt-get update -y
 sudo apt-get install tshark -y
 sudo apt-get install python-pip -y
 
-cd ${WORK_DIR}
+cd ${WORK_DIR} || exit 1
 sudo pip install -r requirements.txt
