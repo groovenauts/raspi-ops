96e60026366fd3400fb3f05835953e7142b8ce55
kumanoryo <r-kumano@groovenauts.jp>
2017-05-14 21:55:42 +0900

    Replace mon-if script and auto exec script, if first deploy.

--- ansible/roles/mon-if/files/srv/mon-if/mon-if.sh
+++ ansible/roles/mon-if/files/srv/mon-if/mon-if.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-WLAN_IF=wlan1
-PHY_IF=phy1
 MON_IF=mon0
+PHY0_MONITOR_MODE=$(/sbin/iw phy phy0 info | grep -c monitor)
+PHY1_MONITOR_MODE=$(/sbin/iw phy phy1 info | grep -c monitor)
 
-sudo ifconfig ${WLAN_IF} down
-sudo iw phy ${PHY_IF} interface add ${MON_IF} type monitor
-sudo ifconfig ${WLAN_IF} up
-sudo ifconfig ${MON_IF} up
+MON_IF_COUNT=$(/sbin/ifconfig | grep -c ${MON_IF})
+if [ "${MON_IF_COUNT}" -eq 0 ]
+then
+	# check phy monitor mode and add mon_if.
+	if [ "${PHY0_MONITOR_MODE}" -gt 0 ]
+	then
+		/sbin/iw phy phy0 interface add ${MON_IF} type monitor
+		/sbin/ifconfig ${MON_IF} up
+	elif [ "${PHY1_MONITOR_MODE}" -gt 0 ]
+	then
+		/sbin/iw phy phy1 interface add ${MON_IF} type monitor
+		/sbin/ifconfig ${MON_IF} up
+	fi
+fi
