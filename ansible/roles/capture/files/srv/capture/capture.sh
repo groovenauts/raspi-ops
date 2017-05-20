ee120113fa710a8905a7b78a0751c94711274f7f
kumanoryo <r-kumano@groovenauts.jp>
2017-05-21 01:04:54 +0900

    Modify capture. Refactor tasks/main.yml file. Add capture user and invite wireshark group. Change file owner root to capture. Change mode 755 to 644 for capture.service. (Don't output tshark warning message.) Change exec user root to capture, and change group root to wireshark. Add stop command and change kill mode.

--- ansible/roles/capture/files/srv/capture/capture.sh
+++ ansible/roles/capture/files/srv/capture/capture.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tshark -i "${INTERFACE}" -T fields -E separator=',' -e frame.time_epoch -e wlan.sa -e radiotap.dbm_antsignal -b filesize:"${FILE_SIZE_KB}" -b files:"${FILES}" -w "${OUTFILE}" 1>/dev/null
+tshark -i "${INTERFACE}" -T fields -E separator=',' -e frame.time_epoch -e wlan.sa -e radiotap.dbm_antsignal -b filesize:"${FILE_SIZE_KB}" -b files:"${FILES}" -w "${OUTFILE} -g" 1>/dev/null
