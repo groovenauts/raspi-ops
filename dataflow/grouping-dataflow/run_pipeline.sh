f21972b6ebdfc880712b15095e9bba7326dae3c5
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2017-05-23 15:27:01 +0900

    Add script to run grouping pipeline on Cloud Dataflow.

---
+++ dataflow/grouping-dataflow/run_pipeline.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+source variables.sh
+
+echo mvn compile exec:java \
+  -Dexec.mainClass=jp.groovenauts.GroupingRssi \
+  -Dexec.args="--project=${PROJECT_ID} --stagingLocation=${STAGING} --pubsubTopic=${INPUT_TOPIC} --outputTopic=${OUTPUT_TOPIC} --runner=BlockingDataflowPipelineRunner"
