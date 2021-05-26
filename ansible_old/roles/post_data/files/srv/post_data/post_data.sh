1cc1261091e76d6f1d1d258fd81f4e1620cdbc8c
kumanoryo <r-kumano@groovenauts.jp>
2021-05-26 11:37:45 +0000

    Replace directory name. Old version of ansible playbook files.

---
+++ ansible_old/roles/post_data/files/srv/post_data/post_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+POST_DATA_PY="${WORK_DIR}/post_data.py"
+HOST_NAME=$(hostname)
+
+if [ -z "${POST_DATA_PY}" ]; then
+  echo "Not found ${POST_DATA_PY}"
+  exit 1
+fi
+
+if [ ! -d "${OUTLOG}" ]; then
+  echo "Not found ${OUTLOG}"
+  exit 1
+fi
+	
+if [ -z "${CONFIG_PATH}" ]; then
+  echo "Not found ${CONFIG_PATH}"
+  exit 1
+fi
+
+if [ -z "${HOST_NAME}" ] ; then
+  echo "Not found ${HOST_NAME}"
+  exit 1
+fi
+
+python -u "${POST_DATA_PY}" "${OUTLOG}" "${HOST_NAME}" "${CONFIG_PATH}"
